12f7889ba3aff30472a5f17d47b5647bf48142b1
Ibragim0055 <165696618+Ibragim0055@users.noreply.github.com>
2024-05-11 14:11:01 +0400

    Update sposob.py

--- sposob.py
+++ sposob.py
@@ -1,4 +1,5 @@
 #Закрытия рекламы
 WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "close_modal_window"))).click()
 
-
+#Нажатия на клавишу Enter при поиска
+wb_search.send_keys(Keys.ENTER)
