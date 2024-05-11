ef2076d8ddaa178fe9dbb4f42b9ffb7735bcd72e
Ibragim0055 <165696618+Ibragim0055@users.noreply.github.com>
2024-05-11 16:51:25 +0400

    Update sposob.py

--- sposob.py
+++ sposob.py
@@ -3,3 +3,11 @@ WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "clo
 
 #Нажатия на клавишу Enter при поиска
 wb_search.send_keys(Keys.ENTER)
+
+
+
+#Чтобы не получать ошибку DevTools listening on ws://127.0.0.1
+options = wd.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+driver = wd.Chrome(options=options)
